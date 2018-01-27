532a6cbf093b7e71edaff5f26c8789246ef0e4c0
Cornell <corvinforeman@gmail.com>
2018-01-26 20:35:02 -0600

    second push

--- foundActivity.js
+++ foundActivity.js
@@ -12,65 +12,18 @@ firebase.initializeApp(config);
 // Create a variable to reference the database.
 var database = firebase.database();
 
-// // Create a reference with an initial file path and name
-// var storage = firebase.storage();
-// var pathReference = storage.ref('images/stars.jpg');
-
-// // Create a reference from a Google Cloud Storage URI
-// var gsReference = storage.refFromURL('gs://bucket/images/stars.jpg')
-
-// // Create a reference from an HTTPS URL
-// // Note that in the URL, characters are URL escaped!
-// var httpsReference = storage.refFromURL('https://firebasestorage.googleapis.com/b/bucket/o/images%20stars.jpg');
-
-
 var activityName;
+var street;
+var city;
+var state;
 var activityType;
 var activityDuration;
 var activityTimeOfDay;
 var activityPrice;
 var activityDescription;
+var streetNumber;
+var streetName;
 
-// $("#upload").on("click", function() {
-//   image = $("#fileToUpload").val().trim();
-
-//   database.ref().push({
-//     image: image,
-//   });
-// });
-
-// whenever a user clicks the button
-$("#add-activity").on("click", function() {
-
-//   // get the input values
-  activityName = $("#name").val().trim();
-  activityType = $("#type").val().trim();
-  activityDuration = $("#duration").val().trim();
-  activityTimeOfDay = $("#timeOfDay").val().trim();
-  activityPrice = $("#price").val().trim();
-  activityDescription = $("#description").val().trim();
-  // image = $("#fileToUpload").val().trim();
-
-
-  // push to firebase
-  database.ref().push({
-    activityName: activityName,
-    activityType: activityType,
-    activityDuration: activityDuration,
-    activityTimeOfDay: activityTimeOfDay,
-    activityPrice: activityPrice,
-    activityDescription: activityDescription,
-  });
-});
-
-database.ref().on("child_added", function(childSnapshot) {
-
-});
-
-
-// 'L3kgSnkgzYty0Xucly6'
-
-// access database
 var ref = database.ref();
 
 ref.on('value', getData, errData);
@@ -82,22 +35,37 @@ ref.on('value', getData, errData);
     var keys = Object.keys(dowhat);
     
 
-    // indexing through the keys but throwing 'undefined';
-    // for(var i = 0; i < keys.length; i++){
-    //   var k = keys[i];
-    //   var activity = dowhat[k].activity;
+    // indexing through the keys;
+    for(var i = 0; i < keys.length; i++){
       
-    //   console.log(activity);
+      let k = keys[i];
+
+      let activityName = dowhat[k].activityName;
+      let activityType = dowhat[k].activityType;
+      let activityDuration = dowhat[k].activityDuration;
+      let activityTimeOfDay = dowhat[k].activityTimeOfDay;
+      let activityDescription = dowhat[k].activityDescription;
+      let streetNumber = dowhat[k].streetNumber;
+      let streetName = dowhat[k].streetName;
+      let city = dowhat[k].city;
+      let state = dowhat[k].state;
+      let activityPrice = dowhat[k].activityPrice;
+ 
+      // will need to place all of this into seperate function.
+      console.log(activityType, activityPrice);
+      $('#activity-name').text(activityName);
+      $('#activity-type').text(activityType);
+      $('#activity-duration').text(activityDuration + " hr(s)");
+      $('#activity-time').text(activityTimeOfDay);
+      $('#activity-description').text(activityDescription);
+      $('#event-location').text(streetNumber + " " + streetName + " " + city + ", " + state);
+      $('#price-range').text("$" + activityPrice);
+
     }
+
   }
 
   function errData(err){
     console.log('Error!');
     console.log(err);
   }
-
-
-
-
-// Google maps
-
